229540fcda79158fd35ace11a8dc8d209d9f5e88
Gbvsp7235 <145117595+Gbvsp7235@users.noreply.github.com>
2026-03-08 16:59:32 +0530

    CHANGES IN SETTINGS

--- sadhak/settings.py
+++ sadhak/settings.py
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'sadhak.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "sadhak_db",
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DATABASE_NAME"),
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
